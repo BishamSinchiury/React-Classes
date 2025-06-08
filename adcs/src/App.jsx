4a4349491970eda68ca110e6ec0a39eccd1d34ac
Bisham <bishamsinchiury1116@gmail.com>
2025-06-08 08:10:50 +0545

    inital

---
+++ adcs/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>This is heading</h1>
+      <p>this is par</p>
+    </>
+  )
+}
+
+export default App
---
+++ adcs/src/Component/nav.jsx
@@ -0,0 +1,16 @@
+import './nav.css'
+function Nav() {
+    
+  return (
+    <>
+      <h1>This is heading 2</h1>
+      <p id='student-1'>Student 1</p>
+      <p className='student-3'>Student 2</p>
+      <p className='student-3'>Student 3</p>
+      <p>Student 4</p>
+    </>
+  )
+}
+
+
+export default Nav
