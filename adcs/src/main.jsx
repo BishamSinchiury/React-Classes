b1074db91dc76ec2186a3a303d6317d4243f5b68
Bisham <bishamsinchiury1116@gmail.com>
2025-06-08 09:34:36 +0545

    add

--- adcs/src/main.jsx
+++ adcs/src/main.jsx
@@ -4,5 +4,16 @@ import './index.css'
 const root = createRoot(document.getElementById('root'))
 
 root.render(
-  
+    <main>
+        <img src="src/assets/react.svg" alt="" />
+        <h1>All of the react Students</h1>
+        <ul>
+            <li>Aavash Sapkota</li>
+            <li>Aakriti Pokheral</li>
+            <li>Aanikit Ganesh</li>
+            <li>Bibek Bishwakarma</li>
+            <li>Pranish Sitoula</li>
+            <li>Sugita Karki</li>
+        </ul>
+    </main>
 )
